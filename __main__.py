57a1ccd34314d9b8c2bcef876977ea5471d5fcff
Simón Villafañe <simonvillafanee@gmail.com>
2025-01-16 12:26:31 -0300

    feat(Root): --pad_start & --pad_end added to the [options]

--- __main__.py
+++ __main__.py
@@ -15,6 +15,8 @@ def print_help():
     print("\t-v, --version  Print the version number")
     print("\t-d, --dir  The directory to iterate through")
     print("\t-p, --prefix  The prefix to use before the (--begin) index")
+    print("\t--pad_start  Pad the name with zeros to the desired length at the beginning of the numbers section")
+    print("\t--pad_end  Pad the name with zeros to the desired length at the end of the numbers section")
     print("\t-b, --begin  The index to start at")
 
 
@@ -22,9 +24,11 @@ if __name__ == "__main__":
     parser = ArgumentParser(add_help=False)
     parser.add_argument("--version", "-v", action="store_true")
     parser.add_argument("--help", "-h", action="store_true")
-    parser.add_argument("--dir", "-d", type=str, default=None, required=True)
-    parser.add_argument("--prefix", "-p", type=str, default="File", required=False)
+    parser.add_argument("--dir", "-d", default=None, required=True)
+    parser.add_argument("--prefix", "-p", default="File", required=False)
     parser.add_argument("--begin", "-b", type=int, default=1, required=False)
+    parser.add_argument("--pad_end", type=int, default=0, required=False)
+    parser.add_argument("--pad_start", type=int, default=0, required=False)
 
     args = parser.parse_args()
 
@@ -33,6 +37,8 @@ if __name__ == "__main__":
     begin: int = args.begin
     directory: str = args.dir
     prefix: str = args.prefix
+    pad_start: int = args.pad_start
+    pad_end: int = args.pad_end
 
     if len(argv[1:]) == 0:
         print("No arguments provided \n")
@@ -49,7 +55,14 @@ if __name__ == "__main__":
 
     for item in Path(directory).iterdir():
         if item.is_file():
-            new_name = f"{prefix}_{begin}{item.suffix}"
+            str_begin = str(begin)
+
+            if pad_start > 0:
+                str_begin = str_begin.rjust(pad_start, "0")
+            if pad_end > 0:
+                str_begin = str_begin.ljust(pad_end, "0")
+
+            new_name = f"{prefix}_{str_begin}{item.suffix}"
             new_path = item.parent / new_name
             begin += 1
 
